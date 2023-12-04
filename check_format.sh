568da45d9f314330c687d603a71021e21281cbc5
darri <darri@Machine.myguest.virtualbox.org>
2023-12-04 18:52:02 +0300

    Добавлены новые изменения

--- check_format.sh
+++ check_format.sh
@@ -6,7 +6,9 @@ expected_format=5
 files=$(find . -type f -name "*.txt")
 
 for file in $iles
+
 do
+    echo "$file"
     if ! grep -q ".\{$expected_format,\}" "$file"; then
         echo "Файл $file не соответствует формату."
         exit 1
